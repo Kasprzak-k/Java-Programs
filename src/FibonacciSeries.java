e47f23675f8439da3ecc6d5a371788e10d5d6f69
Kasprzak-K <kasprzakramos@icloud.com>
2025-09-16 23:49:55 -0300

    + Fibo Series

---
+++ src/FibonacciSeries.java
@@ -0,0 +1,14 @@
+
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = new java.util.Scanner(System.in).nextInt();
+        long a = 0, b = 1;
+        for (int i = 0; i < n; i++){
+            System.out.print(a + (i<n-1?" ":""));
+            long c = a+b;
+            a = b;
+            b = c;
+        }
+    }
+}
